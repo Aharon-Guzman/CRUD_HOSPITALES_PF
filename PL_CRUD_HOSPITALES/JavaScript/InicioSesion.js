976c2547ee25fbf69c78a15219c5c7bda005a7c0
Aharón Guzmán Guzmán <aharongg@hotmail.com>
2025-11-23 23:21:49 -0600

    .

--- PL_CRUD_HOSPITALES/JavaScript/InicioSesion.js
+++ PL_CRUD_HOSPITALES/JavaScript/InicioSesion.js
@@ -199,15 +199,34 @@ function inicioSesion() {
                     $.cookie("GLBCOD", arreglo[2], { expires: TLTC, path: '/', domain: g_Dominio });
                     $.cookie("GLBDSC", arreglo[3], { expires: TLTC, path: '/', domain: g_Dominio });
 
-                    Swal.fire({
-                        position: 'center-center',
-                        icon: 'success',
-                        title: "Inicio de Sesión de Usuario",
-                        text: arreglo[1],
+
+
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: "top-end",
                         showConfirmButton: false,
-                        timer: 4500,
-                        timerProgressBar: true
+                        timer: 3000,
+                        timerProgressBar: true,
+                        didOpen: (toast) => {
+                            toast.onmouseenter = Swal.stopTimer;
+                            toast.onmouseleave = Swal.resumeTimer;
+                        }
                     });
+                    Toast.fire({
+                        icon: "success",
+                        title: "Signed in successfully"
+                    });
+
+
+                    //Swal.fire({
+                    //    position: 'center-center',
+                    //    icon: 'success',
+                    //    title: "Inicio de Sesión de Usuario",
+                    //    text: arreglo[1],
+                    //    showConfirmButton: false,
+                    //    timer: 4500,
+                    //    timerProgressBar: true
+                    //});
 
                     setTimeout(function () {
                         location.href = "../Mantenimientos/frmPrincipal.aspx";
