6cf1317629cd15d1401ab60466c4c335e077dd03
Aharón Guzmán Guzmán <aharongg@hotmail.com>
2025-12-18 18:59:18 -0600

    Actualizacion de DB

---
+++ DB scripts DataSeed.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO [dbo].[Modulos](
+Modulo, ClaseCSS, Enlace
+)VALUES  ('Gestión Hospitales','','frmConsultaHospitales.aspx')
+        ,('Gestión Consultorios','','frmConsultaConsultorios.aspx')
+        ,('Gestión Médicos','','frmConsultaMedicos.aspx')
+        ,('Gestión Pacientes','','frmConsultaPacientes.aspx')
+        ,('Gestión Citas','','frmConsultaCitas.aspx')
+        ,('Catálogo Tipos Identificaciones','','frmConsultaTiposIdentificacion.aspx')
+        ,('Catálogo Tipos Consultorios','','frmConsultaTiposConsultorio.aspx')
+        ,('Catálogo Tipos Especialidades','','frmConsultaTiposEspecialidad.aspx')
+        ,('Catálogo Tipos Citas','','frmConsultaTiposCita.aspx')
+        ,('Administración Usuarios','','frmConsultaUsuarios.aspx')
+        ,('Módulo Auditoría','','')
+
